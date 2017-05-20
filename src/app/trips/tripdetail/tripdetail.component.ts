1b8e32071ff859f3f9a125e16838f697062004cc
sam <sam@ma-work.co.uk>
2017-05-20 13:15:25 +0100

    trip detail page basic structure

--- src/app/trips/tripdetail/tripdetail.component.ts
+++ src/app/trips/tripdetail/tripdetail.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Trip } from '../trip';
+import { TripdataService } from '../tripdata.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-tripdetail',
   templateUrl: './tripdetail.component.html',
-  styleUrls: ['./tripdetail.component.scss']
+  styleUrls: ['./tripdetail.component.scss'],
+  providers:[TripdataService]
 })
 export class TripdetailComponent implements OnInit {
+  trip: Trip;
+  public slug;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private tripdataService: TripdataService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(params => {
+        this.slug = params['slug']
+      });
+    this.trip = this.tripdataService.getTripBySlug(this.slug);
   }
 
 }
